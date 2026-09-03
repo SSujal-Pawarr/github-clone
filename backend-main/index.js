65ed64bba02d278f3ad6dc7dc1fee62031b14f73
SujalPawar <pawarsujal4455@gmail.com>
2026-09-03 23:03:57 +0530

    add yargs and repository controller imports to index.js

--- backend-main/index.js
+++ backend-main/index.js
@@ -7,3 +7,13 @@ const http = require("http");
 const { Server } = require("socket.io");
 const mainRouter = require("./routes/main.router");
 
+const yargs = require("yargs");
+const { hideBin } = require("yargs/helpers");
+
+const { initRepo } = require("./controllers/init");
+const { addRepo } = require("./controllers/add");
+const { commitRepo } = require("./controllers/commit");
+const { pushRepo } = require("./controllers/push");
+const { pullRepo } = require("./controllers/pull");
+const { revertRepo } = require("./controllers/revert");
+
