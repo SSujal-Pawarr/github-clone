a1e644f129abe81777a606e4cfe6a12d56d4d47f
SujalPawar <pawarsujal4455@gmail.com>
2026-09-06 20:33:17 +0530

    add issue routes for CRUD operations

--- backend-main/routes/issue.router.js
+++ backend-main/routes/issue.router.js
@@ -3,3 +3,9 @@ const issueController = require("../controllers/issueController");
 
 const issueRouter = express.Router();
 
+issueRouter.post("/issue/create", issueController.createIssue);
+issueRouter.put("/issue/update/:id", issueController.updateIssueById);
+issueRouter.delete("/issue/delete/:id", issueController.deleteIssueById);
+issueRouter.get("/issue/all", issueController.getAllIssues);
+issueRouter.get("/issue/:id", issueController.getIssueById);
+
