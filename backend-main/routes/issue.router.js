896bcc0d88ce3afd096afbd91dd0cb51f3487d82
SujalPawar <pawarsujal4455@gmail.com>
2026-09-06 20:34:12 +0530

    fix: ensure issueRouter is properly exported

--- backend-main/routes/issue.router.js
+++ backend-main/routes/issue.router.js
@@ -9,3 +9,4 @@ issueRouter.delete("/issue/delete/:id", issueController.deleteIssueById);
 issueRouter.get("/issue/all", issueController.getAllIssues);
 issueRouter.get("/issue/:id", issueController.getIssueById);
 
+module.exports = issueRouter;
