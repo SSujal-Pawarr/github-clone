65c5004e6e03c78d05606ec378a9c06c817ee715
SujalPawar <pawarsujal4455@gmail.com>
2026-09-16 09:32:53 +0530

    add: implement initRepo function to set up repository paths

--- backend-main/controllers/init.js
+++ backend-main/controllers/init.js
@@ -1,4 +1,10 @@
 const fs = require("fs").promises;
 const path = require("path");
 
+async function initRepo() {
+  const repoPath = path.resolve(process.cwd(), ".apnaGit");
+  const commitsPath = path.join(repoPath, "commits");
+
+ 
+}
 
