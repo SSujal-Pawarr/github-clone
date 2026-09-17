d80dcd7fd6a473524c482a02c367d2d54724ea81
SujalPawar <pawarsujal4455@gmail.com>
2026-09-17 08:03:59 +0530

    add: implement createIssue function for issue creation

--- backend-main/controllers/issueController.js
+++ backend-main/controllers/issueController.js
@@ -3,3 +3,23 @@ const Repository = require("../models/repoModel");
 const User = require("../models/userModel");
 const Issue = require("../models/issueModel");
 
+async function createIssue(req, res) {
+  const { title, description } = req.body;
+  const { id } = req.params;
+
+  try {
+    const issue = new Issue({
+      title,
+      description,
+      repository: id,
+    });
+
+    await issue.save();
+
+    res.status(201).json(issue);
+  } catch (err) {
+    console.error("Error during issue creation : ", err.message);
+    res.status(500).send("Server error");
+  }
+}
+
