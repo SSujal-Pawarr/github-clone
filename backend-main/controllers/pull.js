b6952e32eee742ef74e5a12378c4e702569ff211
SujalPawar <pawarsujal4455@gmail.com>
2026-09-19 09:06:55 +0530

    add: implement initial pullRepo function to list commit objects from S3

--- backend-main/controllers/pull.js
+++ backend-main/controllers/pull.js
@@ -2,3 +2,19 @@ const fs = require("fs").promises;
 const path = require("path");
 const { s3, S3_BUCKET } = require("../config/aws-config");
 
+async function pullRepo() {
+  const repoPath = path.resolve(process.cwd(), ".apnaGit");
+  const commitsPath = path.join(repoPath, "commits");
+
+  try {
+    const data = await s3
+      .listObjectsV2({
+        Bucket: S3_BUCKET,
+        Prefix: "commits/",
+      })
+      .promise();
+
+    const objects = data.Contents;
+
+}
+
