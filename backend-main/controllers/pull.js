e6a89220421448e0a9ee155544ea45c1eba38ba1
SujalPawar <pawarsujal4455@gmail.com>
2026-09-19 09:07:16 +0530

    add: implement logic to pull commit files from S3 and save to local directory

--- backend-main/controllers/pull.js
+++ backend-main/controllers/pull.js
@@ -16,5 +16,27 @@ async function pullRepo() {
 
     const objects = data.Contents;
 
+    for (const object of objects) {
+      const key = object.Key;
+      const commitDir = path.join(
+        commitsPath,
+        path.dirname(key).split("/").pop()
+      );
+
+      await fs.mkdir(commitDir, { recursive: true });
+
+      const params = {
+        Bucket: S3_BUCKET,
+        Key: key,
+      };
+
+      const fileContent = await s3.getObject(params).promise();
+      await fs.writeFile(path.join(repoPath, key), fileContent.Body);
+
+      console.log("All commits pulled from S3.");
+    }
+  } catch (err) {
+    console.error("Unable to pull : ", err);
+  }
 }
 
